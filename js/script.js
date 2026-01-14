18039e473ae96d828c84d93ef4faeb7a85e99ae8
ManuelAliquo <manuelaliquo2103@gmail.com>
2026-01-14 18:46:30 +0100

    added timeout for random numbers

--- js/script.js
+++ js/script.js
@@ -9,15 +9,20 @@ const countdownInterval = setInterval(function () {
 }, 1000);
 
 // Creo la lista dei numeri randomici
-const numberslist = document.getElementById("numbers-list");
+const numbersList = document.getElementById("numbers-list");
 
 // Funzione per numero randomico
 const randomNumber = (min, max) => Math.floor(Math.random() * (max - min) + min);
 
-numberslist.innerHTML = `
+numbersList.innerHTML = `
 <li>${randomNumber(1, 50)}</li>
 <li>${randomNumber(1, 50)}</li>
 <li>${randomNumber(1, 50)}</li>
 <li>${randomNumber(1, 50)}</li>
 <li>${randomNumber(1, 50)}</li>
 `;
+
+// Creo il timeOut
+setTimeout(function () {
+  numbersList.innerHTML = "";
+}, 7000);
