51da8b2a5352eb44c33c1bc23498ad398174ef71
ManuelAliquo <manuelaliquo2103@gmail.com>
2026-01-15 18:02:13 +0100

    reforged code + added validation

--- js/script.js
+++ js/script.js
@@ -1,92 +1,108 @@
-// Creo il countdown
+// Recupero gli elementi del DOM
 const countdown = document.getElementById("countdown");
+const instructions = document.getElementById("instructions");
+const randomNumbersList = document.getElementById("numbers-list");
+const answersForm = document.getElementById("answers-form");
+const message = document.getElementById("message");
+const submitBtn = document.querySelector(".btn");
 
-let timeLeft = 6;
-
-const countdownInterval = setInterval(function () {
+// Creo il countdown
+let timeLeft = 4;
+const intervalId = setInterval(function () {
   countdown.innerText = --timeLeft;
-  if (countdown.innerText === "0") clearInterval(countdownInterval);
+  // A timeLeft <= 0:
+  if (timeLeft <= 0) {
+    // il countdown si interrompe e sparisce,
+    clearInterval(intervalId);
+    countdown.style.display = "none";
+    // cambiano le instructions, scompaiono i numeri e appaiono gli input
+    instructions.innerText = "Inserisci i numeri nei riquadri, non importa l'ordine!";
+    randomNumbersList.classList = "d-none";
+    answersForm.classList = "d-block";
+  }
 }, 1000);
 
 // Funzione per numero randomico
-const randomNumber = (min, max) => Math.floor(Math.random() * (max - min) + min);
+const randomNumberGenerator = (min, max) => Math.floor(Math.random() * (max - min) + min);
 
-// Creo la lista dei numeri randomici
-const numbersList = document.getElementById("numbers-list");
+// Array dei numeri usciti
+const randomNumbersArray = [];
 
-numbersList.innerHTML = `
-<li id="number-1">${randomNumber(1, 50)}</li>
-<li id="number-2">${randomNumber(1, 50)}</li>
-<li id="number-3">${randomNumber(1, 50)}</li>
-<li id="number-4">${randomNumber(1, 50)}</li>
-<li id="number-5">${randomNumber(1, 50)}</li>
-`;
+// Finchè l'array non arriva a 5 elementi:
+while (randomNumbersArray.length < 5) {
+  // genero il numero randomico,
+  const randomNumber = randomNumberGenerator(1, 50);
+  // controllo se il numero non è ripetuto,
+  if (!randomNumbersArray.includes(randomNumber)) {
+    // e lo inserisco nell'array
+    randomNumbersArray.push(randomNumber);
+    const listItem = document.createElement("li");
+    listItem.innerText = randomNumber;
+    randomNumbersList.appendChild(listItem);
+  }
+}
 
-const number1 = document.getElementById("number-1");
-const number2 = document.getElementById("number-2");
-const number3 = document.getElementById("number-3");
-const number4 = document.getElementById("number-4");
-const number5 = document.getElementById("number-5");
+// Seleziono tutti gli input
+const inputs = document.querySelectorAll(".form-control");
 
-const randomNumbersArray = [
-  number1.innerText,
-  number2.innerText,
-  number3.innerText,
-  number4.innerText,
-  number5.innerText,
-];
+// Array dei numeri inseriti negli input
+let formInputList = [];
 
-console.log(randomNumbersArray);
+// Contatore di numeri indovinati
+let guessCounter = 0;
 
-const form = document.getElementById("answers-form");
-const instructions = document.getElementById("instructions");
-const message = document.getElementById("message");
+// Verifica per numero
+let duplicatedNumber = 0;
 
-// Creo il timeOut e faccio apparire il form
-setTimeout(() => {
-  countdown.style.display = "none";
-  form.classList = "d-block";
-  numbersList.innerHTML = "";
-  instructions.innerText = "Scrivi i numeri nei riquadri, non importa in che ordine!";
-}, 7000);
-
-// Controllo i numeri inseriti
-const formInput1 = document.getElementById("form-input-1");
-const formInput2 = document.getElementById("form-input-2");
-const formInput3 = document.getElementById("form-input-3");
-const formInput4 = document.getElementById("form-input-4");
-const formInput5 = document.getElementById("form-input-5");
-
-const formInputValues = [];
-
-const submitButton = document.querySelector(".btn");
-
-let outputMsg;
-
-let counter = 0;
-
-submitButton.addEventListener("click", function (event) {
+// Al click del submit:
+answersForm.addEventListener("submit", function (event) {
+  // evito che si ricarichi la pagina,
   event.preventDefault();
-  formInputValues.push(
-    formInput1.value,
-    formInput2.value,
-    formInput3.value,
-    formInput4.value,
-    formInput5.value
-  );
-  console.log(formInputValues);
-  submitButton.disabled = true;
-  for (let i = 0; i < formInputValues.length; i++) {
-    const currentInputValue = formInputValues[i];
-    if (randomNumbersArray.includes(currentInputValue)) {
-      counter++;
-      outputMsg = `Hai indovinato ${counter} numeri`;
-      message.innerText = outputMsg;
+  // inserisco gli input nell'array,
+  for (let i = 0; i < inputs.length; i++) {
+    const currentInputValue = inputs[i].value;
+    // se il numero è già presente nell'array viene segnalato in un contatore apposito
+    if (formInputList.includes(currentInputValue)) {
+      ++duplicatedNumber;
+    }
+    // altrimenti viene inserito nell'array
+    else {
+      formInputList.push(currentInputValue);
     }
   }
-  if (outputMsg == undefined) {
-    outputMsg = "Non hai indovinato nessun numero";
-    message.innerText = outputMsg;
+
+  // Comparazione dei numeri
+  for (let i = 0; i < formInputList.length; i++) {
+    const currentInputNumber = formInputList[i];
+    for (let i = 0; i < randomNumbersArray.length; i++) {
+      const currentRandomNumber = randomNumbersArray[i];
+      // Se il numero nell'input è uguale a uno dei numeri random aggiungo +1 al guessCounter
+      if (currentInputNumber == currentRandomNumber) ++guessCounter;
+    }
   }
-  console.log(outputMsg);
+
+  // Se il contatore dei duplicati è salito:
+  if (duplicatedNumber > 0) {
+    message.innerText = "Hai inserito più volte lo stesso numero!";
+    // svuoto l'array, e azzero il guessCounter
+    formInputList = [];
+    guessCounter = 0;
+    // 1 secondo dopo azzero il conteggio dei duplicati
+    const intervalId = setInterval(function () {
+      duplicatedNumber = 0;
+      if (duplicatedNumber === 0) clearInterval(intervalId);
+    }, 1000);
+  }
+  // Messaggio di risultato in base al valore del guessCounter:
+  if (duplicatedNumber === 0) {
+    if (guessCounter === 0) {
+      message.innerText = "Non hai indovinato nessun numero!";
+    } else if (guessCounter === 1) {
+      message.innerText = `Hai indovinato 1 numero!`;
+      message.classList = "text-warning text-center";
+    } else if (guessCounter > 1) {
+      message.innerText = `Hai indovinato ${guessCounter} numeri!`;
+      message.classList = "text-success text-center";
+    }
+  }
 });
